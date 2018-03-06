2fa0489e652a8f19972d429944c9f3f90d81e95e
Flae <flae@mail.com>
2018-03-06 00:04:08 -0500

    Initial commit

---
+++ src/ABCBot/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ABCBot
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ test/ABCBot.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ABCBot.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
