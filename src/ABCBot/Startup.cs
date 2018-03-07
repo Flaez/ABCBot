1ebf69b6d986a862b4f5b315877ddffe4043cac5
Flae <flae@tutanota.com>
2018-03-07 07:08:56 -0500

    Create generic function for executing a pipeline on a specific issue

--- src/ABCBot/Startup.cs
+++ src/ABCBot/Startup.cs
@@ -62,5 +62,35 @@ namespace ABCBot
 
             return Task.CompletedTask;
         }
+
+        private async Task ProcessIssue(int issueId) {
+            var dataDirectory = Configuration["DataDirectory"];
+
+            using (var scope = Services.CreateScope()) {
+                var services = scope.ServiceProvider;
+
+                var masterRepository = new MasterRepository(services.GetService<IGitHubService>(), services.GetService<IGitService>(), dataDirectory);
+
+                await masterRepository.Initialize();
+
+                using (var repositoryContext = await masterRepository.CreateContext(issueId)) {
+                    var merchantDetailsLoader = services.GetService<IMerchantDetailsLoader>();
+                    var merchantDetailsOption = await merchantDetailsLoader.ExtractDetails(issueId);
+
+                    var merchantDetails = merchantDetailsOption.ValueOrFailure();
+
+                    var pipelineContext = new PipelineContext(issueId, merchantDetails, repositoryContext);
+
+                    var pipeline = ABCPipelineFactory.BuildStandardPipeline(pipelineContext, services);
+
+                    Log.Information("Starting pipeline for id: {id}", issueId);
+
+                    var result = await pipeline.Process();
+
+                    Log.Information("Pipeline completed: {result}", result);
+                }
+            }
+        }
+
     }
 }
